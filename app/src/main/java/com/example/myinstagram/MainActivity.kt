6339dc8f56b29e1deac32782f9691602c6e43139
오지원 <64193469+ojw4184@users.noreply.github.com>
2020-07-14 15:51:20 +0900

    Photo Upload

--- app/src/main/java/com/example/myinstagram/MainActivity.kt
+++ app/src/main/java/com/example/myinstagram/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.myinstagram
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import com.example.myinstagram.navigation.AlarmFragment
-import com.example.myinstagram.navigation.DetailViewFragment
-import com.example.myinstagram.navigation.GridFragment
-import com.example.myinstagram.navigation.UserFragment
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.myinstagram.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
     }
 
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
@@ -31,6 +34,8 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
             R.id.action_add_photo ->{
 
+                if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
                 return true
             }
             R.id.action_favorite_alarm ->{
