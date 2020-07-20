47a5090756476359ded3e9841359a04e51e97d6c
오지원 <64193469+ojw4184@users.noreply.github.com>
2020-07-21 01:25:17 +0900

    Click Profile image / Logout button

--- app/src/main/java/com/example/myinstagram/LoginActivity.kt
+++ app/src/main/java/com/example/myinstagram/LoginActivity.kt
@@ -60,6 +60,10 @@ class LoginActivity : AppCompatActivity() {
         callbackManager = CallbackManager.Factory.create()
     }
 
+    override fun onStart() {
+        super.onStart()
+        moveMainPage(auth?.currentUser)
+    }
 
     fun printHashKey() {
         try {
@@ -174,6 +178,7 @@ class LoginActivity : AppCompatActivity() {
     fun moveMainPage(user:FirebaseUser?) {
         if (user != null){
             startActivity(Intent(this,MainActivity::class.java))
+            finish()
         }
     }
 
--- app/src/main/java/com/example/myinstagram/MainActivity.kt
+++ app/src/main/java/com/example/myinstagram/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.myinstagram.navigation.*
@@ -25,6 +26,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     }
 
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        setToolbarDefault()
         when(p0.itemId){
             R.id.action_home ->{
                 var detailViewFragment = DetailViewFragment()
@@ -58,5 +60,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
         }
         return false
+    }
+    fun setToolbarDefault(){
+        toolbar_username.visibility = View.GONE
+        toolbar_btn_back.visibility = View.GONE
+        toolbar_title_image.visibility = View.VISIBLE
     }
 }
